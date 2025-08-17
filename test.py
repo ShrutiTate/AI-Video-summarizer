ecdbc4d874a53d533d6c0168ac51be26db2778b6
ShrutiTate <shrutitate2602004@gmail.com>
2025-08-17 20:10:41 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,1 @@
-import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyBaSmyIZrGjFOzzouktd0ZDzbMNDik7L_o")
-model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("Hello Gemini!")
-print(response.text)
